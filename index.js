d953bd74ed089a3dc39928021a0aa0d9e9013c2a
Bill Ramsey <billramsey@gmail.com>
2016-07-05 20:22:20 -0700

    Moves global environment to it's own file and implements static analysis

--- index.js
+++ index.js
@@ -2,153 +2,53 @@
 const Promise = require('bluebird');
 const parser = require('./parser');
 const Browser = require('zombie');
+const globalEnv = require('./env.js');
 
-// TODO:
-// tests
-// delay 3
-// for loop?
-// variables.
-// ( { ?
-//
-//  Implement scoping
-// write a script to test our own site
+const validityCheck = (actionList, gEnv) => {
+  const isValid = (tree, errors, env) => {
+    for (let i = 0; i < tree.length; i++) {
+      const action = tree[i];
+      if (action.type === 'CallExpression') {
+        if (env[action.operator] === undefined || typeof env[action.operator] !== 'function') {
+          errors.push(`${action.operator} is not a function`);
+        }
+        if (action.params !== undefined) {
+          validityCheck(action.params, errors, env);
+        }
+      } else if (action.type === 'function') {
+        // dummy action to assist in analysis.
+        env[action.operator] = () => true;
+      }
+    }
+    return errors;
+  };
+  //  create a sub env because we will mutate the env to add functions defined in the tree.
+  const subEnv = Object.create(gEnv);
+  return isValid(actionList, [], subEnv);
+};
 
-
-// get /
-// fill username bill
-// fill password password
-// pressButton Login
-// get /links
-
-// set x 3
-// while(lte $x 4) {
-// set x (add $x 1)
-// log $x
-// }
-// log outofloop
-
-const scriptText = `
-set x 3
-while(lte $x 4) {
-  set x (add $x 1)
-  log $x
-}
-`;
 //
 // Test the parse of the string.  Returns an object with
 // {success: true || false }
 // If there is a failure, inlcude error, line and column in return object
 const parseTest = (str) => {
+  let actions = [];
   try {
-    parser.parse(str.trim());
-    return { success: true };
+    actions = parser.parse(str.trim());
   } catch (err) {
-    return {
-      success: false,
-      error: err.message,
-      line: err.location.start.line,
-      column: err.location.start.column,
-    };
+    throw new SyntaxError(`${err.message} at 
+      line ${err.location.start.line} 
+      and column ${err.location.start.column}`);
   }
-};
+  const errors = validityCheck(actions, globalEnv);
+  if (errors.length > 0) {
+    throw new SyntaxError(`${errors.toString()}`);
+  }
 
-const globalEnv = {
-  responseTimes: [],
-  mult: (arg1, arg2, env) => new Promise((resolve, reject) => resolve(arg1 * arg2)),
-  add: (arg1, arg2, env) => new Promise((resolve, reject) => resolve(+arg1 + +arg2)),
-  eq: (arg1, arg2, env) => new Promise((resolve, reject) => resolve(arg1 === arg2)),
-  lt: (arg1, arg2, env) => new Promise((resolve, reject) => resolve(arg1 < arg2)),
-  gt: (arg1, arg2, env) => new Promise((resolve, reject) => resolve(arg1 > arg2)),
-  lte: (arg1, arg2, env) => new Promise((resolve, reject) => resolve(arg1 <= arg2)),
-  gte: (arg1, arg2, env) => new Promise((resolve, reject) => resolve(arg1 >= arg2)),
-  log: (arg1, env) => new Promise((resolve, reject) => resolve(console.log(arg1))),
-  set: (variableName, primitiveValue, env) => {
-    return new Promise((resolve, reject) => {
-      if (variableName in env) {
-        let searchEnv = env;
-        while (!Object.prototype.hasOwnProperty.call(searchEnv, variableName)) {
-          searchEnv = Object.getPrototypeOf(searchEnv);
-        }
-        searchEnv[variableName] = primitiveValue;
-      } else {
-        env[variableName] = primitiveValue;
-      }
-      return resolve();
-    });
-  },
-  times: (times, expr, env) => {
-    // console.log('expr', expr);
-    return Promise.resolve(times).then((n) => {
-      const promiseList = [];
-      for (let i = 0; i < n; i++) {
-        promiseList.push(expr);
-      }
-      return Promise.resolve(promiseList);
-    })
-    .mapSeries((a) => {
-      a.type = 'CallExpression';
-      return evaluate(a, env);
-    })
-    .then((added) => Promise.resolve(env.browser));
-  },
-  get: (path, env) =>
-    new Promise((resolve, reject) => {
-      // console.log('visiting:', path);
-      const startTime = new Date();
-      env.browser.visit(path).then((blah) => {
-        const endTime = new Date();
-        env.responseTimes.push({
-          actionName: `get ${path}`,
-          path: env.browser.request.url,
-          statusCode: env.browser.response.status,
-          elapsedTime: endTime - startTime,
-          httpVerb: env.browser.request.method,
-        });
-        resolve(env.browser);
-      });
-    }),
-  fill: (selector, value, env) =>
-    new Promise((resolve, reject) => {
-      // console.log('filling:', selector, 'with ', value);
-      env.browser.fill(selector, value);
-      resolve(env.browser);
-    }),
-  pressButton: (selector, env) =>
-    new Promise((resolve, reject) => {
-      // console.log('pressing:', selector);
-      const startTime = new Date();
-      env.browser.pressButton(selector).then(() => {
-        const endTime = new Date();
-        env.responseTimes.push({
-          actionName: `pressButton ${selector}`,
-          path: env.browser.request.url,
-          statusCode: env.browser.response.status,
-          elapsedTime: endTime - startTime,
-          httpVerb: env.browser.request.method,
-        });
-        resolve(env.browser);
-      });
-    }),
+  return actions;
 };
-
-// Creating a while loop with promises is an ugly affair.
-const promiseLoop = (action, env) => {
-  // console.log(action);
-  return Promise.resolve(evaluate(action.operator, env)).then(function loop(bool) {
-    //  console.log(i);
-    // console.log('bool:', bool);
-    const subEnv = Object.create(env);
-    if (bool) {
-      return Promise.mapSeries(action.params, (a) => evaluate(a, subEnv))
-        .then(() => Promise.resolve(evaluate(action.operator, env)))
-        .then(loop);
-    }
-    return Promise.resolve();
-  });
-};
-
 const evaluate = (action, env) => {
-  console.log('action', action);
+  // console.log('action', action);
   if (action.type === 'primitive') {
     return Promise.resolve(action.value);
   }
@@ -201,28 +101,57 @@ const evaluate = (action, env) => {
   return Promise.reject(`Unknown type: '${action.type}' in ${action}`);
 };
 
-const run = (host, script) => {
-  globalEnv.browser = new Browser({ site: host });
-  const actions = parser.parse(script.trim());
+
+// Creating a while loop with promises is an ugly affair.
+const promiseLoop = (action, env) => {
+  // console.log(action);
+  return Promise.resolve(evaluate(action.operator, env)).then(function loop(bool) {
+    //  console.log(i);
+    // console.log('bool:', bool);
+    const subEnv = Object.create(env);
+    if (bool) {
+      return Promise.mapSeries(action.params, (a) => evaluate(a, subEnv))
+        .then(() => Promise.resolve(evaluate(action.operator, env)))
+        .then(loop);
+    }
+    return Promise.resolve();
+  });
+};
+
+const run = (host, script, env) => {
+  // Add some things to global environment
+  env.browser = new Browser({ site: host });
+  env.responseTimes = [];
+
+  const actions = parseTest(script.trim());
   //  console.log(actions);
   const scenarioStart = new Date();
   return Promise.mapSeries(actions,
     (action) => {
       // console.log('evaluating:', action.operator.name);
-      return evaluate(action, globalEnv).then((ret) => {
+      return evaluate(action, env).then((ret) => {
       }).catch((e) => console.log('unresolved error', e));
     }).then((r) => {
       const scenarioEnd = new Date();
       const times = {
         scenarioTime: scenarioEnd - scenarioStart,
-        transactionTimes: globalEnv.responseTimes,
+        transactionTimes: env.responseTimes,
       };
       return Promise.resolve(times);
     })
     .catch((fail) => console.log('failed for reason', fail));
 };
 
-run('http://localhost:3000', scriptText)
+const scriptText = `
+set x 1
+func bob(hi) {
+  test
+}
+get /
+`;
+
+// console.log(parseTest(scriptText));
+run('http://localhost:3000', scriptText, globalEnv)
 .then((data) => console.log('data:', data))
 .catch((err) => console.log('err', err));
 
